efac2d650c18fa3b54cfe9d59c8877ddb7563038
Anton Tayanovskyy <anton@pulumi.com>
2023-05-19 14:50:37 -0400

    Ensure no panics in the ProgramTest underlying provider

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,4 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+//go:build nodejs || all
 // +build nodejs all
 
 package examples
@@ -24,6 +25,16 @@ func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
 			"@pulumi/auth0",
+		},
+
+		// Temporary profilactic check until pulumi/pulumi#12981 is resolved.
+		ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+			for _, e := range stack.Events {
+				eventsJSON, err := json.MarshalIndent(e, "", "  ")
+				assert.NoError(t, err)
+				assert.NotContainsf(t, string(eventsJSON), "panic",
+					"Unexpected panic recorded in engine events")
+			}
 		},
 	})
 
