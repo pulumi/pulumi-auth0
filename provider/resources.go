10666e7df505d48ad456164ae0b52927e608ff61
Venelin <venelin@pulumi.com>
2024-06-13 13:41:32 +0100

    test prc

--- provider/resources.go
+++ provider/resources.go
@@ -48,9 +48,9 @@ var metadata []byte
 
 func Provider() tfbridge.ProviderInfo {
 	p := shimv2.NewProvider(auth0Shim.NewProvider(),
-		shimv2.WithPlanResourceChange(func(tfResourceType string) bool {
-			return tfResourceType == "auth0_connection"
-		})
+		shimv2.WithPlanResourceChange(func(_ string) bool {
+			return true
+		}),
 	)
 
 	prov := tfbridge.ProviderInfo{
